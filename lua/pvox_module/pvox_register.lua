850718baf254a73d5fec7165b5377e02530fde79
Kai D. Gonzalez <gkai70263@gmail.com>
2024-08-03 13:59:32 -0400

    Make sure that modules are clientside added

--- lua/pvox_module/pvox_register.lua
+++ lua/pvox_module/pvox_register.lua
@@ -2,7 +2,7 @@
 --*
 --* this behaviour is already registered as a module, but however, not as a playermodel bind.
 
-if SERVER then
-	PVox:RegisterPlayerModel("models/player/combine_soldier.mdl", "combinesoldier")
-	PVox:RegisterPlayerModel("models/player/combine_soldier_prisonguard.mdl", "combinesoldier")
-end
+AddCSLuaFile()
+
+PVox:RegisterPlayerModel("models/player/combine_soldier.mdl", "combinesoldier")
+PVox:RegisterPlayerModel("models/player/combine_soldier_prisonguard.mdl", "combinesoldier")

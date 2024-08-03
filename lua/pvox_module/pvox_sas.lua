7872cd447a0c459e672ec04f5fda87abc459fc08
Kai D. Gonzalez <gkai70263@gmail.com>
2024-08-03 13:59:35 -0400

    Make sure that modules are clientside added

--- lua/pvox_module/pvox_sas.lua
+++ lua/pvox_module/pvox_sas.lua
@@ -1,3 +1,3 @@
-if SERVER then
-	PVox:RegisterPlayerModel("models/arachnit/csgonewsas/ctm_sasplayer.mdl", "cs2-sas")
-end
+AddCSLuaFile()
+
+PVox:RegisterPlayerModel("models/arachnit/csgonewsas/ctm_sasplayer.mdl", "cs2-sas")

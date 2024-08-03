ae4eecd1a11c430437126445e420fc61e5776b6e
Kai D. Gonzalez <gkai70263@gmail.com>
2024-08-03 13:59:45 -0400

    Make sure that modules are clientside added

--- lua/pvox_module/pvox_ter.lua
+++ lua/pvox_module/pvox_ter.lua
@@ -1,3 +1,3 @@
-if SERVER then
-	PVox:RegisterPlayerModel("models/arachnit/csgonewphoenix/tm_phoenix_variantaplayer.mdl", "cs2-phoenix")
-end
+AddCSLuaFile()
+
+PVox:RegisterPlayerModel("models/arachnit/csgonewphoenix/tm_phoenix_variantaplayer.mdl", "cs2-phoenix")

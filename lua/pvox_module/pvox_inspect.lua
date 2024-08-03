e727a9e2809f704455693f98cd0001f467fd0e43
Kai D. Gonzalez <gkai70263@gmail.com>
2024-08-03 13:59:29 -0400

    Make sure that modules are clientside added

--- lua/pvox_module/pvox_inspect.lua
+++ lua/pvox_module/pvox_inspect.lua
@@ -14,26 +14,27 @@
 --* the client realm as well as the server.
 --*
 
+AddCSLuaFile()
+
 if SERVER then
 	util.AddNetworkString("PVox_Inspect")
-
-	if ! PVox then
-		-- for mods, this is recommended
-		-- as it lets the user know the PVOX API is not
-		-- installed. This is not required, but should be considered
-		-- if you want to make an addon that scales easily to other environments
-		-- as well as allows for easier error handling.
-		Derma_Message("The PVOX Module API is not established! This could mean you do not have the PVox module installed.", "PVox Error", "OK")
-	end
-
-	-- we're using a singular net message here,
-	-- there's a lot of other ways to do this
-	-- this is the one i found the most efficient
-	net.Receive("PVox_Inspect", function(len, ply)
-		local pmod = PVox:GetModule(ply:GetNWString("vox_preset", "none"))
-		pmod:EmitAction(ply, "inspect")
-	end)
 end
+if ! PVox then
+	-- for mods, this is recommended
+	-- as it lets the user know the PVOX API is not
+	-- installed. This is not required, but should be considered
+	-- if you want to make an addon that scales easily to other environments
+	-- as well as allows for easier error handling.
+	Derma_Message("The PVOX Module API is not established! This could mean you do not have the PVox module installed.", "PVox Error", "OK")
+end
+
+-- we're using a singular net message here,
+-- there's a lot of other ways to do this
+-- this is the one i found the most efficient
+net.Receive("PVox_Inspect", function(len, ply)
+	local pmod = PVox:GetModule(ply:GetNWString("vox_preset", "none"))
+	pmod:EmitAction(ply, "inspect")
+end)
 
 if CLIENT then
 	hook.Add("KeyPress", "PVox_Inspect", function(ply, key)

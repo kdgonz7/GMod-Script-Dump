607899e8a6ba27741428556bf6f640d563a52ed8
Kai D. Gonzalez <gkai70263@gmail.com>
2024-08-05 12:24:13 -0400

    updated the script to remove the .ogg files

--- sound/pvox/ninetailedfox/a.py
+++ sound/pvox/ninetailedfox/a.py
@@ -5,3 +5,4 @@ for path,di,files in os.walk("."):
     for i in files:
         if i.endswith(".ogg"):
             os.system("ffmpeg -i " + str(path) + "/" + str(i) + " -af \"aformat=sample_fmts=s16:sample_rates=44100\" " + str(path) + "/" + str(i)[:-4] + ".wav")
+            os.remove(str(path) + "/" + str(i))
